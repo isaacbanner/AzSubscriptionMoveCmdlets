31346044d78c311754c4516c211cdcda4f064c0f
Hengxiu Gao <henggao@microsoft.com>
2023-09-11 13:14:22 -0700

    Adding ps to download akv info

---
+++ Backup-AzureKeyVaults.ps1
@@ -0,0 +1,21 @@
+Import-Module Az.KeyVault
+
+function Get-AllAzureKeyVaults () {
+    
+    $akvOutputFilePath = ".\akvInfo.json"
+    $allAkvs = Get-AzKeyVault
+    $resultAkvList = New-Object System.Collections.ArrayList
+    Write-Output "Start downloading Azure KeyVault information ..."
+
+    foreach ($akv in $allAkvs) {
+        $kv = Get-AzKeyVault -ResourceGroupName $akv.ResourceGroupName -VaultName $akv.VaultName
+        $resultAkvList.Add($kv) | out-null
+        Write-Output ("Finished downloading {0} / {1}" -f $resultAkvList.Count, $allAkvs.Count)
+    }
+
+    # Save retrieved akv in a json file
+    $resultAkvList | ConvertTo-Json | Out-File -FilePath $akvOutputFilePath
+}
+
+# Connect-AzAccount -Subscription $SubscriptionId
+Get-AllAzureKeyVaults
