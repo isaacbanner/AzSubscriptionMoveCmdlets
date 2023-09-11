f257dde870255192c8b70150a2d77035aeb2f1f6
Isaac Sarek Banner <isaacba@microsoft.com>
2023-09-11 11:54:08 -0700

    Add fetch of UA and SA identities

---
+++ Backup-AzIdentitiesAndResources.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Reads all identities and identity-enabled resources from a subscription and dumps to a file.
+.PARAMETER Subscription
+    The subscription to dump.
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]
+    $Subscription
+)
+
+function Get-UserContext ($Subscription) {  
+    $context = Get-AzContext
+
+    if ($null -eq $context -or $Subscription -ne $context.Subscription.Id)
+    {
+        Connect-AzAccount -Subscription $Subscription
+        $context = Get-AzContext
+    }
+
+    return $context
+}
+
+function Get-AllSystemAssignedIdentities ($Subscription)
+{
+    $ArgIdentities = Search-AzGraph -Query 'Resources | where subscriptionId =~ "fa5fc227-a624-475e-b696-cdd604c735bc" | where isnotempty(identity) | where identity.["type"] has "SystemAssigned" | project id'
+    return $ArgIdentities | % {Get-AzSystemAssignedIdentity -Scope $_.id}
+}
+
+$userAssigned = Get-AzUserAssignedIdentity -SubscriptionId $Subscription
+$systemAssigned = Get-AllSystemAssignedIdentities($Subscription)
+
+return $systemAssigned + $userAssigned
