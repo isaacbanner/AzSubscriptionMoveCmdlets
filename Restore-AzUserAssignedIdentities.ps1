50e588bf4e80c9b270a7b2cd0bc93a54dd1a09b4
Isaac Sarek Banner <isaacba@microsoft.com>
2023-09-11 16:15:01 -0700

    Delete and recreate a list of identities

---
+++ Restore-AzUserAssignedIdentities.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    Reads all identities and identity-enabled resources from a subscription and dumps to a file.
+.PARAMETER Subscription
+    The subscription to dump.
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]
+    $Subscription,
+    
+    [Parameter(Mandatory=$True)]
+    [string]
+    $TenantId,
+
+    [Parameter(Mandatory=$True)]
+    [psobject[]]
+    $Identities
+)
+
+function Get-UserContext ($Subscription, $TenantId) {  
+    $context = Get-AzContext
+
+    if ($null -eq $context -or $Subscription -ne $context.Subscription.Id)
+    {
+        Connect-AzAccount -Subscription $Subscription -TenantId $TenantId
+        $context = Get-AzContext
+    }
+
+    return $context
+}
+
+function Restore-AzSingleIdentity($identity)
+{
+    Get-AzUserAssignedIdentity -ResourceGroupName $identity.ResourceGroupName -Name $identity.Name | Remove-AzUserAssignedIdentity
+    return New-AzUserAssignedIdentity -ResourceGroupName $identity.ResourceGroupName -Name $identity.Name -Location $identity.Location
+}
+
+$context = Get-UserContext -Subscription $Subscription -TenantId $TenantId
+
+$Identities | % {
+    Restore-AzSingleIdentity($_)
+}
