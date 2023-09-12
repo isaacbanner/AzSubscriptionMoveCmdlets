1c9a58d5769cbd6962c862783dda9a06758fcedb
Isaac Banner <isaacba@microsoft.com>
2023-09-12 13:07:48 -0700

    Update with GET

--- AzRestMethodTools.psm1
+++ AzRestMethodTools.psm1
@@ -3,18 +3,6 @@
     PS Module with multiple Az Rest Method utilities
 #>
 
-function Get-ApiVersionsForProvider ([string] $ResourceProvider, [string] $ResourceType)
-{
-    $providerResponse = Invoke-AzRestMethod -Path "/providers/$($ResourceProvider)?api-version=2023-07-01"
-    $provider = ConvertFrom-Json $providerResponse.Content
-    $resourceTypeDefinition = $provider.resourceTypes | Where-Object { $ResourceType -eq $_.resourceType }
-
-    return [PSCustomObject]@{
-        apiVersions = $resourceTypeDefinition.apiVersions
-        defaultApiVersion = $resourceTypeDefinition.defaultApiVersion
-    }
-}
-
 function Get-UserContext ($Subscription, $TenantId) {  
     $context = Get-AzContext
 
@@ -25,4 +13,24 @@ function Get-UserContext ($Subscription, $TenantId) {
     }
 
     return $context
+}
+
+function Get-AzApiVersionsForProvider ([string] $ResourceProvider, [string] $ResourceType)
+{
+    $providerResponse = Invoke-AzRestMethod -Path "/providers/$($ResourceProvider)?api-version=2023-07-01"
+    $provider = ConvertFrom-Json $providerResponse.Content
+    $resourceTypeDefinition = $provider.resourceTypes | ? { $ResourceType -eq $_.resourceType }
+
+    return [PSCustomObject]@{
+        apiVersions = $resourceTypeDefinition.apiVersions
+        defaultApiVersion = $resourceTypeDefinition.defaultApiVersion
+    }
+}
+
+function Get-AzResourceDefinition($Resource)
+{
+    $apiVersions = Get-AzApiVersionsForProvider -ResourceProvider $Resource.ResourceProvider -ResourceType $Resource.ResourceType
+    $response = Invoke-AzRestMethod -Method GET -ApiVersion $apiVersions.defaultApiVersion -ResourceGroupName $resource.resourceGroupName -Name $resource.name -ResourceProviderName $resource.resourceProvider -ResourceType $resource.resourceType 
+
+    return ConvertFrom-Json $response.Content
 }
