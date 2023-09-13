0b1da90fcbace9756de05076ab25749981d8db09
Isaac Banner <isaacba@microsoft.com>
2023-09-13 09:07:31 -0700

    Add trunk module

---
+++ Az.SubscriptionTenantMigration.psm1
@@ -0,0 +1,5 @@
+<#
+.SYNOPSIS
+    Powershell module to assist in the migration of subscriptions between two AAD tenants.
+    Includes tooling to backup identity and RBAC configuration then restore that configuration in the new tenant.
+#>
